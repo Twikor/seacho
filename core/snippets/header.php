4e0336f14c3461ca97aa645e920e547b42c8f547
孙海倍 <shb@sunhb.me>
2017-02-08 13:07:56 +0800

    Update header.php

--- core/snippets/header.php
+++ core/snippets/header.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="zh_CN">
+ <head> 
+  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+  <meta charset="UTF-8" /> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+  <title>紫沫的博客</title> 
+  <meta name="description" content="" /> 
+  <link rel="stylesheet" type="text/css" href="/bl-themes/skeleton/css/normalize.css" /> 
+  <link rel="stylesheet" type="text/css" href="/bl-themes/skeleton/css/skeleton.css" /> 
+  <link rel="stylesheet" type="text/css" href="/bl-themes/skeleton/css/blog.css" /> 
+ </head> 
+ <body> 
+  <div id="layout" class="container"> 
