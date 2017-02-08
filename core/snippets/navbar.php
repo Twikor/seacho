c5c9789d805d63b4331aa861db62f57cdca7e35f
孙海倍 <shb@sunhb.me>
2017-02-08 13:09:29 +0800

    Update navbar.php

--- core/snippets/navbar.php
+++ core/snippets/navbar.php
@@ -1,1 +1,31 @@
-
+   <!-- Sidebar --> 
+   <nav class="one-third column"> 
+    <!-- Plugins Sidebar --> 
+    <div class="plugin plugin-about">
+     <h2>关于</h2>
+     <div class="plugin-content"></div>
+    </div>
+    <div class="plugin plugin-latest-posts">
+     <h2 class="plugin-title">文章</h2>
+     <div class="plugin-content">
+      <ul>
+       <li><a href="/post/test">test</a></li>
+      </ul>
+     </div>
+    </div>
+    <div class="plugin plugin-pages">
+     <h2>页面</h2>
+     <div class="plugin-content">
+      <ul class="parents">
+       <li><a class="parent active" href="/">首页</a></li>
+       <li class="parent"><a class="parent" href="/about">关于</a></li>
+      </ul>
+     </div>
+    </div>
+    <div class="plugin plugin-tags">
+     <h2>标签</h2>
+     <div class="plugin-content">
+      <ul></ul>
+     </div>
+    </div> 
+   </nav> 
